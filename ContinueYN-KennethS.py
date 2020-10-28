4c4d5b3a4ff08d8d21e0183e005cbf096ad03850
Phantom-mp4 <73188105+Phantom-mp4@users.noreply.github.com>
2020-10-28 12:33:39 -0500

    Add files via upload

---
+++ ContinueYN-KennethS.py
@@ -0,0 +1,6 @@
+Answer = str(input("Would you like to continue? "))
+
+if Answer == "Yes" or "Y" or "OK" or "Why not" or "Sure":
+    print("Ok")
+if Answer == "No" or "N":
+    print("Bad Input.")
---
+++ Grades-KennethS.py
@@ -0,0 +1,12 @@
+GradeLetter = str(input("What is your grade letter(A-F)? "))
+
+if GradeLetter == "A":
+    print("The numeric value is 4")
+if GradeLetter == "B":
+    print("The numeric value is 3")
+if GradeLetter == "C":
+    print("The numeric value is 2")
+if GradeLetter == "D":
+    print("The numeric value is 1")
+if GradeLetter == "F":
+    print("The numeric value is 0")
