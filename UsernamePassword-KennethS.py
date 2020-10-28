831a6067d2d75e7e05846f479e687f3b237413d5
Phantom-mp4 <73188105+Phantom-mp4@users.noreply.github.com>
2020-10-28 12:12:56 -0500

    Add files via upload

---
+++ UsernamePassword-KennethS.py
@@ -0,0 +1,6 @@
+Username = str(input("Please enter your username. "))
+
+Password = str(input("Please enter your password. "))
+
+if Username == "Admin" and Password == "Password":
+    print("Succesful Login.")
